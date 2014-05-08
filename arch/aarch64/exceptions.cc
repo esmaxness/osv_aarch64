janikokkonen/osv

aarch64: exceptions: implement interrupt table
implement a simple interrupt table with descriptors
which record interrupt IDs, handlers and interrupt type.

Signed-off-by: Claudio Fontana <claudio.fontana@huawei.com>

arch/aarch64/exceptions.cc
@@ -1,0 +1,103 @@
+/*
+ * Copyright (C) 2014 Huawei Technologies Duesseldorf GmbH
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+#include <osv/debug.hh>
+#include <osv/prio.hh>
+#include <osv/sched.hh>
+
+#include "exceptions.hh"
+#include "gic.hh"
+
+__thread exception_frame* current_interrupt_frame;
+class interrupt_table interrupt_table __attribute__((init_priority((int)init_prio::idt)));
+
+interrupt_table::interrupt_table() {
+    debug_early_entry("interrupt_table::interrupt_table()");
+
+    /* XXX hardcoded addresses */
+    gic::gic = new gic::gic_driver(0x8001000, 0x8002000);
+    gic::gic->init_cpu(0);
+    gic::gic->init_dist(0);
+
+    this->nr_irqs = gic::gic->nr_irqs;
+
+    debug_early("interrupt table: gic driver created.\n");
+}
+
+void interrupt_table::enable_irqs()
+{
+    WITH_LOCK(osv::rcu_read_lock) {
+        for (int i = 0; i < this->nr_irqs; i++) {
+            struct interrupt_desc *desc = this->irq_desc[i].read();
+            if (desc && desc->handler) {
+                debug_early_u64("enabling InterruptID=", desc->id);
+                gic::gic->set_irq_type(desc->id, desc->type);
+                gic::gic->unmask_irq(desc->id);
+            }
+        }
+    }
+}
+
+void interrupt_table::register_handler(int i, interrupt_handler h,
+                                       gic::irq_type t)
+{
+    WITH_LOCK(_lock) {
+        assert(i < this->nr_irqs);
+        struct interrupt_desc *old = this->irq_desc[i].read_by_owner();
+
+        if (old) {
+            debug_early_u64("already registered IRQ id=", (u64)i);
+            return;
+        }
+
+        struct interrupt_desc *desc = new interrupt_desc(i, h, t);
+        this->irq_desc[i].assign(desc);
+        osv::rcu_dispose(old);
+
+        debug_early_u64("registered IRQ id=", (u64)i);
+    }
+}
+
+int interrupt_table::invoke_interrupt(int id)
+{
+    WITH_LOCK(osv::rcu_read_lock) {
+        assert(id < this->nr_irqs);
+        struct interrupt_desc *desc = this->irq_desc[id].read();
+
+        if (!desc || !desc->handler) {
+            return 0;
+        }
+
+        desc->handler(desc);
+        return 1;
+    }
+}
+
+extern "C" { void interrupt(exception_frame* frame); }
+
+void interrupt(exception_frame* frame)
+{
+    /* remember frame in a global, need to change if going to nested */
+    current_interrupt_frame = frame;
+
+    unsigned int iar = gic::gic->ack_irq();
+    int irq = iar & 0x3ff;
+
+    /* note that special values 1022 and 1023 are used for
+       group 1 and spurious interrupts respectively. */
+    if (irq >= gic::gic->nr_irqs) {
+        printf("special InterruptID %x detected!\n");
+
+    } else {
+        if (!interrupt_table.invoke_interrupt(irq))
+            printf("unhandled InterruptID %x!\n", irq);
+        gic::gic->end_irq(iar);
+    }
+
+    current_interrupt_frame = nullptr;
+    sched::preempt();
+}

arch/aarch64/exceptions.hh
@@ -17,6 +17,8 @@
 #include <osv/mutex.h>
 #include <vector>
 
+#include "gic.hh"
+
 struct exception_frame {
     u64 regs[31];
     u64 sp;
@@ -32,28 +34,32 @@ struct exception_frame {
 
 extern __thread exception_frame* current_interrupt_frame;
 
-struct shared_vector {
-    unsigned vector;
-    unsigned id;
-    shared_vector(unsigned v, unsigned i)
-        : vector(v), id(i)
-    {};
+typedef void (*interrupt_handler)(struct interrupt_desc *);
+
+struct interrupt_desc {
+    interrupt_desc(int i, interrupt_handler h, gic::irq_type t)
+        : id(i), handler(h), type(t) {}
+
+    int id;
+    interrupt_handler handler;
+    gic::irq_type type;
 };
 
 class interrupt_table {
 public:
     interrupt_table();
-    void load_on_cpu();
-    unsigned register_handler(std::function<void ()> handler);
-    // The pre_eoi should 'true' when the interrupt is for the device, 'false' otherwise.
-    shared_vector register_level_triggered_handler(unsigned gsi, std::function<bool ()> pre_eoi, std::function<void ()> handler);
-    void unregister_level_triggered_handler(shared_vector v);
-    unsigned register_interrupt_handler(std::function<bool ()> pre_eoi, std::function<void ()> eoi, std::function<void ()> handler);
-    void unregister_handler(unsigned vector);
-    void invoke_interrupt(unsigned vector);
+    void enable_irqs();
+
+    void register_handler(int id, interrupt_handler h, gic::irq_type t);
+    int invoke_interrupt(int id); /* returns 0 if no handler registered */
+
+protected:
+    int nr_irqs; /* number of supported InterruptIDs, read from gic */
+    osv::rcu_ptr<struct interrupt_desc> irq_desc[gic::max_nr_irqs];
+    mutex _lock;
 };
 
-extern interrupt_table interrupt_table;
+extern class interrupt_table interrupt_table;
 
 extern "C" {
     void page_fault(exception_frame* ef);

esmaxness/osv_aarch64

arch/aarch64/exceptions.cc
@@ -1,0 +1,103 @@
+/*
+ * Copyright (C) 2014 Huawei Technologies Duesseldorf GmbH
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+#include <osv/debug.hh>
+#include <osv/prio.hh>
+#include <osv/sched.hh>
+
+#include "exceptions.hh"
+#include "gic.hh"
+
+__thread exception_frame* current_interrupt_frame;
+class interrupt_table idt __attribute__((init_priority((int)init_prio::idt)));
+
+interrupt_table::interrupt_table() {
+    debug_early_entry("interrupt_table::interrupt_table()");
+
+    /* XXX hardcoded addresses */
+    gic::gic = new gic::gic_driver(0x8001000, 0x8002000);
+    gic::gic->init_cpu(0);
+    gic::gic->init_dist(0);
+
+    this->nr_irqs = gic::gic->nr_irqs;
+
+    debug_early("interrupt table: gic driver created.\n");
+}
+
+void interrupt_table::enable_irqs()
+{
+    WITH_LOCK(osv::rcu_read_lock) {
+        for (int i = 0; i < this->nr_irqs; i++) {
+            struct interrupt_desc *desc = this->irq_desc[i].read();
+            if (desc && desc->handler) {
+                debug_early_u64("enabling InterruptID=", desc->id);
+                gic::gic->set_irq_type(desc->id, desc->type);
+                gic::gic->unmask_irq(desc->id);
+            }
+        }
+    }
+}
+
+void interrupt_table::register_handler(int i, interrupt_handler h,
+                                       gic::irq_type t)
+{
+    WITH_LOCK(_lock) {
+        assert(i < this->nr_irqs);
+        struct interrupt_desc *old = this->irq_desc[i].read_by_owner();
+
+        if (old) {
+            debug_early_u64("already registered IRQ id=", (u64)i);
+            return;
+        }
+
+        struct interrupt_desc *desc = new interrupt_desc(i, h, t);
+        this->irq_desc[i].assign(desc);
+        osv::rcu_dispose(old);
+
+        debug_early_u64("registered IRQ id=", (u64)i);
+    }
+}
+
+int interrupt_table::invoke_interrupt(int id)
+{
+    WITH_LOCK(osv::rcu_read_lock) {
+        assert(id < this->nr_irqs);
+        struct interrupt_desc *desc = this->irq_desc[id].read();
+
+        if (!desc || !desc->handler) {
+            return 0;
+        }
+
+        desc->handler(desc);
+        return 1;
+    }
+}
+
+extern "C" { void interrupt(exception_frame* frame); }
+
+void interrupt(exception_frame* frame)
+{
+    /* remember frame in a global, need to change if going to nested */
+    current_interrupt_frame = frame;
+
+    unsigned int iar = gic::gic->ack_irq();
+    int irq = iar & 0x3ff;
+
+    /* note that special values 1022 and 1023 are used for
+       group 1 and spurious interrupts respectively. */
+    if (irq >= gic::gic->nr_irqs) {
+        printf("special InterruptID %x detected!\n");
+
+    } else {
+        if (!idt.invoke_interrupt(irq))
+            printf("unhandled InterruptID %x!\n", irq);
+        gic::gic->end_irq(iar);
+    }
+
+    current_interrupt_frame = nullptr;
+    sched::preempt();
+}

arch/aarch64/exceptions.hh
@@ -17,6 +17,8 @@
 #include <osv/mutex.h>
 #include <vector>
 
+#include "gic.hh"
+
 struct exception_frame {
     u64 regs[31];
     u64 sp;
@@ -32,28 +34,32 @@ struct exception_frame {
 
 extern __thread exception_frame* current_interrupt_frame;
 
-struct shared_vector {
-    unsigned vector;
-    unsigned id;
-    shared_vector(unsigned v, unsigned i)
-        : vector(v), id(i)
-    {};
+typedef void (*interrupt_handler)(struct interrupt_desc *);
+
+struct interrupt_desc {
+    interrupt_desc(int i, interrupt_handler h, gic::irq_type t)
+        : id(i), handler(h), type(t) {}
+
+    int id;
+    interrupt_handler handler;
+    gic::irq_type type;
 };
 
 class interrupt_table {
 public:
     interrupt_table();
-    void load_on_cpu();
-    unsigned register_handler(std::function<void ()> handler);
-    // The pre_eoi should 'true' when the interrupt is for the device, 'false' otherwise.
-    shared_vector register_level_triggered_handler(unsigned gsi, std::function<bool ()> pre_eoi, std::function<void ()> handler);
-    void unregister_level_triggered_handler(shared_vector v);
-    unsigned register_interrupt_handler(std::function<bool ()> pre_eoi, std::function<void ()> eoi, std::function<void ()> handler);
-    void unregister_handler(unsigned vector);
-    void invoke_interrupt(unsigned vector);
+    void enable_irqs();
+
+    void register_handler(int id, interrupt_handler h, gic::irq_type t);
+    int invoke_interrupt(int id); /* returns 0 if no handler registered */
+
+protected:
+    int nr_irqs; /* number of supported InterruptIDs, read from gic */
+    osv::rcu_ptr<struct interrupt_desc> irq_desc[gic::max_nr_irqs];
+    mutex _lock;
 };
 
-extern interrupt_table interrupt_table;
+extern class interrupt_table idt;
 
 extern "C" {
     void page_fault(exception_frame* ef);